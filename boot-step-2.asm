a1b8b4307d981f8449b2b427bc3c5bd963bfc20c
ennghallie <hernandezjhosh2014@gmail.com>
2024-07-11 15:56:50 +0800

    ndone

--- boot-step-2.asm
+++ boot-step-2.asm
@@ -1,1 +1,17 @@
 bits 16
+
+bs2:
+push dx
+mov dx,0
+mov cx,0
+mov ax,510
+mov bx,keyword1
+call inputspecific
+pop dx
+push dx
+mov dx,0
+mov cx,0
+mov ax,0
+mov bx,keyword2
+call inputspecific
+pop dx
